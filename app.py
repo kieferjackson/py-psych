ee01ee5b9169c3e814a526a496a943233705ec53
Kiefer Jackson <kieferleejackson@gmail.com>
2023-09-17 17:27:54 -0700

    Updated POST params + logic to extract resp data

--- app.py
+++ app.py
@@ -1,39 +1,67 @@
 import json
+import re
 import pycurl
+import os
 from io import BytesIO
 
-api = json.load(open('api.json'))
+IMG_FOLDER_NAME = 'img'
+image_filenames = os.listdir('./' + IMG_FOLDER_NAME)
 
-def make_optiic_request (api_key, filename):
+api = json.load(open('api.json'))
+API_KEY = api['key']
+API_URL = api['url']
+
+def make_optiic_request (filename):
     # Create the curl request object
     buffer = BytesIO()
     c = pycurl.Curl()
-    url = api['url']
 
-    c.setopt(c.URL, url)
+    c.setopt(c.URL, API_URL)
     c.setopt(c.POST, 1)
-    c.setopt(c.POSTFIELDS, 'apiKey=' + api_key)
     c.setopt(c.WRITEDATA, buffer)
-    c.setopt(c.HTTPPOST, [("image", (c.FORM_FILE, 'img/' + filename))])
+    c.setopt(c.HTTPPOST, [
+        ('apiKey', API_KEY),
+        ("image", (c.FORM_FILE, IMG_FOLDER_NAME + '/' + filename))
+    ])
     c.setopt(pycurl.HTTPHEADER, ['Accept-Language: en'])
     c.perform()
-    print("POST '" + filename + "' to:", url)
+    print("POST '" + filename + "' to:", API_URL)
     c.close()
 
     # Get the response value
     response = buffer.getvalue()
-    print(response.decode("utf-8"))
+    resp = response.decode("utf-8")
     # Parse the response and get the returned text
-    respObj = json.loads(response)
+    respObj = json.loads(resp)
     text = respObj['text']
     print(text)
 
     return text
 
+# Get index card data and associate based on its collection ID and its face
+index_card_data = {}
+for filename in image_filenames:
+    # Get the collection ID and face from the first numeral and following character
+    # NOTE: a = Front ; b = Back
+    collection_data = re.findall(r'(\d+|[A-Za-z]+)', filename)
+    card_id, card_face, *junk = collection_data
+    key_value = 'front' if card_face == 'a' else 'back'
 
-# TEST
-API_KEY = api['key']
-text1 = make_optiic_request(API_KEY, '1.jpg')
-text2 = make_optiic_request(API_KEY, '2.jpg')
+    # POST the file by its filename to Optiic to get the raw text data
+    raw_text = make_optiic_request(filename)
+    stripped_text = raw_text.replace('\n', ' ')
+    text_data = stripped_text.split('%@&')
+    print(text_data)
 
-print(text1, '\n', text2, sep='')
+    if index_card_data.get(card_id) == None:
+        # Initialize the data property
+        index_card_data[card_id] = { 'data': [] }
+
+    for i, text in enumerate(text_data):
+        # Check if entry already exists at given index (i), and append empty dict if necessary
+        if i >= len(index_card_data[card_id]['data']):
+            index_card_data[card_id]['data'].append({})
+
+        index_card_data[card_id]['data'][i][key_value] = text
+    
+print(index_card_data)
